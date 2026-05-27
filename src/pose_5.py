b740e8bb240d7fe8294dcd8c4115e2511428adb4
Victor van Faassen <v.l.v.faassen@student.tue.nl>
2026-05-27 21:21:23 +0200

    9.1_only

--- src/pose_5.py
+++ src/pose_5.py
@@ -1,15 +1,18 @@
 import numpy as np
+import copy
 from helperfunctions import add_pose_from_global, add_landmark_measurement_from_global
 import gtsam
 from gtsam.symbol_shorthand import L, X
 
-PRIOR_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.1, 0.1, 0.05]))  # (x, y, theta)
-ODOMETRY_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.2, 0.2, 0.1]))  # (dx, dy, dtheta)
-MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (bearing, range)
+PRIOR_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.1, 0.1, 0.05]))
+ODOMETRY_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.2, 0.2, 0.1]))
+MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))
+
 
 def add_pose(graph, initial_estimate, pose_5):
-    # Adding the initial estimate for the 5th pose using our helper function `add_pose_from_global` which also adds the odometry factor between X(4) and X(5).
+
     pose_4 = initial_estimate.atPose2(X(4))
+
     graph, initial_estimate = add_pose_from_global(
         graph=graph,
         initial_estimate=initial_estimate,
@@ -19,11 +22,14 @@ def add_pose(graph, initial_estimate, pose_5):
         new_pose_global=pose_5,
         odom_noise=ODOMETRY_NOISE
     )
+
     return graph, initial_estimate
 
+
 def add_landmark_measurement(graph, result, pose_5, landmark):
-    # Adding the measurement from X(5) to the chosen landmark using our helper function `add_landmark_measurement_from_global` which calculates the correct bearing and range from the global poses.``
+
     landmark_point = result.atPoint2(L(landmark))
+
     graph = add_landmark_measurement_from_global(
         graph=graph,
         pose_key=X(5),
@@ -32,20 +38,23 @@ def add_landmark_measurement(graph, result, pose_5, landmark):
         landmark_point=landmark_point,
         measurement_noise=MEASUREMENT_NOISE
     )
+
     return graph
 
+
 def optimize(graph, initial_estimate):
-    # TODO: Initialize the optimizer 
 
-
-    # TODO: Perform the optimization and print the result
+    params = gtsam.LevenbergMarquardtParams()
+    optimizer = gtsam.LevenbergMarquardtOptimizer(graph, initial_estimate, params)
+    result = optimizer.optimize()
 
     return result
 
+
 def minimize_marginals(graph, initial_estimate, pose_options):
     #TODO: try different pose and landmark options here, and keep the one with the lowest sum of marginals.
-    best_pose = "a"      # chosen pose option
-    best_landmark = 1    # chosen landmark (1 or 2)
+    best_pose = "d"
+    best_landmark = 1
     pose_5 = pose_options[best_pose]
     graph, initial_estimate = add_pose(graph, initial_estimate, pose_5)
     result = optimize(graph, initial_estimate)
@@ -53,10 +62,11 @@ def minimize_marginals(graph, initial_estimate, pose_options):
     result = optimize(graph, initial_estimate)
 
     # TODO: Calculate marginal covariances for the relevant variables and visualize the updated factor graph with covariances
-    marginals = []
+    marginals = gtsam.Marginals(graph, result)
     # The sum of the marginals for each landmark can be computed using marginals.marginalCovariance(L(x)).sum()
-    sum_of_marginals = 0
+    sum_of_marginals = marginals.marginalCovariance(L(1)).sum() + marginals.marginalCovariance(L(2)).sum()
     return best_pose, best_landmark, sum_of_marginals
+
 
 def minimize_errors(graph, initial_estimate, pose_options):
     #TODO: try different pose and landmark options here, and keep the one with the lowest resulting error.
